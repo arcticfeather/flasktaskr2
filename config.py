a9250277f020eab4ebdc187275376547e6f4a693
Michael Browne <arcticfeather@hotmail.com>
2014-08-28 15:55:43 +0900

    fixd config.py

--- config.py
+++ config.py
@@ -4,13 +4,12 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 DATABASE = 'flasktaskr.db'
-<<<<<<< HEAD
+
 CSRF_ENABLED = True
 SECRET_KEY = 'c8e88e3032ac40'
 DEBUG = False
-=======
+
 SECRET_KEY = 'c8e88e3032ac40'
->>>>>>> 4a19f85f75cb5013e7c7d72ca685d6f61b254e0c
 
 # defines the full path for the database
 DATABASE_PATH = os.path.join(basedir, DATABASE)
