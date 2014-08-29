9f9a99d5b49512ed397ab11202ebde8997db6782
Michael Browne <arcticfeather@hotmail.com>
2014-08-29 13:56:54 +0900

    Read-only API implmented

--- project/views.py
+++ project/views.py
@@ -1,5 +1,6 @@
 from project import app, db
-from flask import flash, redirect, session, url_for, render_template, request
+from project.models import Task
+from flask import flash, redirect, session, url_for, render_template, request, jsonify, make_response
 from functools import wraps
 import datetime
 
@@ -23,9 +24,47 @@ def flash_errors(form):
 ####### ROUTES #######
 
 @app.route('/', defaults={'page': 'index'})
-
 def index(page):
 	return(redirect(url_for('tasks.tasks')))
+
+@app.route('/api/tasks/', methods=['GET'])
+def tasks():
+	if request.method == 'GET':
+		results = db.session.query(Task).limit(10).offset(0).all()
+		json_results = []
+		for result in results:
+			data = {
+				'task id': result.task_id,
+				'task name': result.name,
+				'due date': str(result.due_date),
+				'priority': result.priority,
+				'posted date': str(result.posted_date),
+				'status': result.status,
+				'user id': result.user_id
+			}
+			json_results.append(data)
+	return jsonify(items=json_results)
+
+@app.route('/api/tasks/<int:task_id>')
+def task(task_id):
+	if request.method == 'GET':
+		result = db.session.query(Task).filter_by(task_id=task_id).first()
+		if result:
+			json_result = {
+				'task id': result.task_id,
+				'task name': result.name,
+				'due date': str(result.due_date),
+				'priority': result.priority,
+				'posted date': str(result.posted_date),
+				'status': result.status,
+				'user id': result.user_id
+			}
+			code = 200
+		else:
+			json_result = {"sorry": "Element does not exist"}
+			code = 404
+		return make_response(jsonify(json_result), code)
+
 
 @app.errorhandler(404)
 def page_not_found(error):
